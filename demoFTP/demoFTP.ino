e799163fcaa12aa3e69e0ff24444915272b8de18
Nmbwj <84346757+Nmbwj@users.noreply.github.com>
2024-06-29 11:46:05 +0300

    work on post

--- demoFTP/demoFTP.ino
+++ demoFTP/demoFTP.ino
@@ -1,41 +1,28 @@
-String url = "\"eu-central-1.sftpcloud.io\"";
-
-String username = "\"21bcb027c0ae4a40a055385b4f97c321\"";
-String password = "\"Wm6XB8B8tFjFNYPY4I8OPDSdTgd9NqVJ\"";
 
 
 void setup() {
   // Setup for Serial communication
   Serial.begin(115200);
   Serial3.begin(9600);
-  
-  sendFtp(url);
+  String url = "\"eu-central-1.sftpcloud.io\"";
+  String username = "\"4c9f4fb87fbb4c599e13cd6f637985ea\"";
+  String password = "\"9QajNJs9xa67guInVk75HqSl1bTtni57\"";
+  setupFTP(url, username, password);
+  sendGETFtp();
+  delay(15000);
+  setupFTP(url, username, password);
+  sendPUTFtp();
   
 
 }
 
 void loop(){
+  
 
 }
 
-void sendFtp(String url)
+void sendGETFtp()
 {
-  // THIS IS FTP
-  //START  CONFIGURATION AND COMMUNICATION
-  sendATCommand("AT+FTPCID=1");  
-  delay(3000);
-  //sendATCommand("AT+FTPPORT="+port+""); //port assign
-  //delay(1000);
-  //sendATCommand("AT+FTPPORT?"); //check port
-  //delay(1000);
-  //sendATCommand("AT+FTPMODE?"); // check mode of communication whether active or passive
-  //delay(1000);
-  sendATCommand("AT+FTPSERV="+url+"");
-  delay(3000);
-  sendATCommand("AT+FTPUN="+username+"");
-  delay(3000);
-  sendATCommand("AT+FTPPW="+password+"");
-  delay(3000);
   sendATCommand("AT+FTPGETNAME=\"enginecut.txt\"");
   delay(5000);
   sendATCommand("AT+FTPGETPATH=\"/\"");
@@ -44,6 +31,35 @@ void sendFtp(String url)
   delay(5000);
   sendATCommand("AT+FTPGET=2,1024");
   delay(5000);
+  
+  
+}
+void sendPUTFtp()
+{
+  sendATCommand("AT+FTPPUTNAME=\"starteng.txt\"");
+  delay(5000);
+  sendATCommand("AT+FTPPUTPATH=\"/\"");
+  delay(5000);
+  sendATCommand("AT+FTPPUT=1");
+  delay(5000);
+  sendATCommand("AT+FTPPUT=2,100");
+  delay(5000);
+  sendATCommand("Hello");
+  delay(5000);
+  sendATCommand("AT+FTPPUT=2,0");
+  delay(5000);
+}
+void setupFTP(String url, String username, String password){
+  // THIS IS FTP
+  //START  CONFIGURATION AND COMMUNICATION
+  sendATCommand("AT+FTPCID=1");  
+  delay(3000);
+  sendATCommand("AT+FTPSERV="+url+"");
+  delay(3000);
+  sendATCommand("AT+FTPUN="+username+"");
+  delay(3000);
+  sendATCommand("AT+FTPPW="+password+"");
+  delay(3000);
   
   
 }
