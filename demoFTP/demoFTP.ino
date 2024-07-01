e28bf9a747ce888ab871df9c2cf8ef1815871e7a
Nmbwj <84346757+Nmbwj@users.noreply.github.com>
2024-07-01 15:31:06 +0300

    PUT and GET through FTPS

--- demoFTP/demoFTP.ino
+++ demoFTP/demoFTP.ino
@@ -1,5 +1,5 @@
 
-
+String message= "Hello sam!\r";
 void setup() {
   // Setup for Serial communication
   Serial.begin(115200);
@@ -31,6 +31,9 @@ void sendGETFtp()
   delay(5000);
   sendATCommand("AT+FTPGET=2,1024");
   delay(5000);
+  //CLOSE GPRS CONTEX
+  sendATCommand("AT+SAPBR =0,1");
+  delay(1000);
   
   
 }
@@ -42,16 +45,26 @@ void sendPUTFtp()
   delay(5000);
   sendATCommand("AT+FTPPUT=1");
   delay(5000);
-  sendATCommand("AT+FTPPUT=2,100");
-  delay(5000);
-  sendATCommand("Hello");
+  sendUAT("AT+FTPPUT=2,"+String(message.length()-1)+"\r", message);
   delay(5000);
   sendATCommand("AT+FTPPUT=2,0");
   delay(5000);
+  //CLOSE GPRS CONTEX
+  sendATCommand("AT+SAPBR =0,1");
+  delay(1000);
 }
 void setupFTP(String url, String username, String password){
   // THIS IS FTP
   //START  CONFIGURATION AND COMMUNICATION
+  //START GPRS BARRIER
+  sendATCommand("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");  
+  delay(3000);
+  sendATCommand("AT+SAPBR=3,1,\"APN\",\"etc.com\""); // APN configuration
+  delay(1000);
+  sendATCommand("AT+SAPBR=1,1"); // APN configuration
+  delay(1000);
+  sendATCommand("AT+SAPBR=2,1"); // GPRS connectivity.
+  delay(1000);
   sendATCommand("AT+FTPCID=1");  
   delay(3000);
   sendATCommand("AT+FTPSERV="+url+"");
@@ -64,13 +77,28 @@ void setupFTP(String url, String username, String password){
   
 }
 
+
 void sendATCommand(String cmd)
 {
   Serial3.println(cmd);
   delay(1000); // Wait for the command to be processed
   updateSerial();
 }
+void sendUAT(String cmd, String data){
+  int i =0;
+  while(cmd.charAt(i)){
+    Serial3.write(cmd.charAt(i++));
+  }
+  i=0;
+  delay(5000);
+  while(data.charAt(i)){
+    Serial3.write(data.charAt(i++));
+  }  
+  delay(1000);
+  updateSerial();
 
+
+}
 
 void updateSerial()
 {
